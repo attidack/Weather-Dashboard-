840b816b146345041d0c18e4dc0628f1ea1dad57
Jon Bird <attid@Jons-Laptop>
2022-06-12 13:44:52 -0600

    tutoring session at office hours

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ let cityName
 let currentUVspan
 let currentUVData
 let iconurl
+let listOfCities = []
 
 
 // clear page function to reset data for each search
@@ -55,7 +56,7 @@ const fetchApi = (lat,lon) => {
         // date from api pull
         var date = new Date(data.current.dt * 1000)
         var todaysDate = date.getDate()
-        var quarymonth = date.getMonth()
+        var quarymonth = date.getMonth() +1;
         var quaryYear = date.getFullYear()
         let cityNameSpan = $('<span>').text(cityName + ' (' + quarymonth + '/'+ todaysDate + '/' + quaryYear + ')')
         cityNameDiv.append(cityNameSpan)
@@ -92,10 +93,18 @@ const fetchApi = (lat,lon) => {
         const forcastDivTitle = $('<h2>').addClass('forcast').text('5-Day Forcast:')
         const forcastContainerDiv = $('<div>').addClass('flex-row justify-space-between')
         
+//         const cars = ["BMW", "Volvo", "Mini"];
+// let text = "";
+
+// for (let x of cars) {
+//   text += x + " ";
+// }
+
+
         // forcast day 1
         const day1 = $('<div>').addClass('forcast-card col-12 col-md-2')
         const day1Date = new Date(data.daily[0].dt * 1000)
-        const day1Title = $('<div>').text(day1Date.getMonth() +'/'+ day1Date.getDay() + '/' + day1Date.getFullYear())
+        const day1Title = $('<div>').text((day1Date.getMonth()+1) +'/'+ day1Date.getDate() + '/' + day1Date.getFullYear())
         const day1iconcode = data.daily[0].weather[0].icon
         iconurl = "http://openweathermap.org/img/w/" + day1iconcode + ".png";
         const day1WeatherIcon = $('<img>').attr('src', iconurl).attr('alt', 'weather Icon')
@@ -108,7 +117,7 @@ const fetchApi = (lat,lon) => {
         // forcast day 2
         const day2 = $('<div>').addClass('forcast-card col-12 col-md-2')
         const day2Date = new Date(data.daily[1].dt * 1000)
-        const day2Title = $('<div>').text(day2Date.getMonth() +'/'+ day2Date.getDay() + '/' + day2Date.getFullYear())
+        const day2Title = $('<div>').text(day2Date.getMonth() +'/'+ day2Date.getDate() + '/' + day2Date.getFullYear())
         const day2iconcode = data.daily[1].weather[0].icon
         iconurl = "http://openweathermap.org/img/w/" + day2iconcode + ".png";
         const day2WeatherIcon = $('<img>').attr('src', iconurl).attr('alt', 'weather Icon')
@@ -122,7 +131,7 @@ const fetchApi = (lat,lon) => {
         // forcast day 3
         const day3 = $('<div>').addClass('forcast-card col-12 col-md-2')
         const day3Date = new Date(data.daily[2].dt * 1000)
-        const day3Title = $('<div>').text(day3Date.getMonth() +'/'+ day3Date.getDay() + '/' + day3Date.getFullYear())
+        const day3Title = $('<div>').text(day3Date.getMonth() +'/'+ day3Date.getDate() + '/' + day3Date.getFullYear())
         const day3iconcode = data.daily[2].weather[0].icon
         iconurl = "http://openweathermap.org/img/w/" + day3iconcode + ".png";
         const day3WeatherIcon = $('<img>').attr('src', iconurl).attr('alt', 'weather Icon')
@@ -135,7 +144,7 @@ const fetchApi = (lat,lon) => {
         // forcast day 4
         const day4 = $('<div>').addClass('forcast-card col-12 col-md-2')
         const day4Date = new Date(data.daily[3].dt * 1000)
-        const day4Title = $('<div>').text(day4Date.getMonth() +'/'+ day4Date.getDay() + '/' + day4Date.getFullYear())
+        const day4Title = $('<div>').text(day4Date.getMonth() +'/'+ day4Date.getDate() + '/' + day4Date.getFullYear())
         const day4iconcode = data.daily[3].weather[0].icon
         iconurl = "http://openweathermap.org/img/w/" + day4iconcode + ".png";
         const day4WeatherIcon = $('<img>').attr('src', iconurl).attr('alt', 'weather Icon')
@@ -148,7 +157,7 @@ const fetchApi = (lat,lon) => {
         // forcast day 5
         const day5 = $('<div>').addClass('forcast-card col-12 col-md-2')
         const day5Date = new Date(data.daily[4].dt * 1000)
-        const day5Title = $('<div>').text(day5Date.getMonth() +'/'+ day5Date.getDay() + '/' + day5Date.getFullYear())
+        const day5Title = $('<div>').text(day5Date.getMonth() +'/'+ day5Date.getDate() + '/' + day5Date.getFullYear())
         const day5iconcode = data.daily[0].weather[0].icon
         iconurl = "http://openweathermap.org/img/w/" + day5iconcode + ".png";
         const day5WeatherIcon = $('<img>').attr('src', iconurl).attr('alt', 'weather Icon')
@@ -170,11 +179,38 @@ const fetchApi = (lat,lon) => {
         console.log()
     })
 };
+// on click search for cityname
 $('.btn').click(function(e){
     e.preventDefault()
     cityName = $('#City').val()
-    
-
-    console.log(cityName)
     locationLookup(cityName)
+    handleSave(cityName)
 });
+
+// Save city to local host
+const handleSave = (cityName) => { 
+    listOfCities = listOfCities.filter(city => city.toLowerCase() !== cityName.toLowerCase()); 
+    listOfCities.push(cityName.toLowerCase());
+    // how the filter works
+    /* let tempArray = [];
+    for(let i=0; i<listOfCities.length; i++) {
+        let city = listOfCities[i];
+        if (city !== cityName) {
+            tempArray.push(city);
+        }
+    }
+    listOfCities = tempArray;*/
+    localStorage.setItem('history', JSON.stringify(listOfCities))
+};
+
+// load saved history from localhost
+const load = () => {
+    let cityName = localStorage.getItem('history')
+    if (!cityName) {
+        listOfCities = []
+    } else {
+        listOfCities = JSON.parse(cityName)
+    }
+    console.log(cityName)
+};
+load()
